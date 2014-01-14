8dc1fc4fd0155b2d6c14fcb3f6eb4045a468ce12
Buga Dániel <daniel@bugadani.hu>
2014-01-14 20:20:43 +0100

    Fix code relying on magic calls.

--- Module.php
+++ Module.php
@@ -16,17 +16,18 @@ class Module extends \Miny\Modules\Module
 
     public function init(BaseApplication $app)
     {
-        $app->add('markdown', __NAMESPACE__ . '\Markdown');
+        $factory = $app->getFactory();
+        $factory->add('markdown', __NAMESPACE__ . '\Markdown');
 
-        $this->ifModule('Cache', function()use($app) {
-            $app->getBlueprint('markdown')
+        $this->ifModule('Cache', function() use($factory) {
+            $factory->getBlueprint('markdown')
                     ->setArguments('&cache');
         });
 
-        $this->ifModule('Templating', function()use($app) {
-            $app->add('markdown_function', '\Modules\Templating\Compiler\Functions\CallbackFunction')
+        $this->ifModule('Templating', function() use($factory) {
+            $factory->add('markdown_function', '\Modules\Templating\Compiler\Functions\CallbackFunction')
                     ->setArguments('markdown', '*markdown::format', array('is_safe' => true));
-            $app->getBlueprint('template_environment')
+            $factory->getBlueprint('template_environment')
                     ->addMethodCall('addFunction', '&markdown_function');
         });
     }
